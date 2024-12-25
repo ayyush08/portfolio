c08e2ecd1be02fe571401a6c2d01e0d77451333a
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-12-25 22:53:56 +0530

    refactor Main component layout and integrate TextGenerateEffect for dynamic text display

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,34 +1,36 @@
 'use client'
 import React from 'react'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
-import Image from 'next/image'
+import { TextGenerateEffect } from './ui/text-generate-effect';
+
 const Main = () => {
     return (
         <>
-            
-                <div className='flex  mx-auto  items-center justify-center gap-10 p-10 '>
-                    <div className='border-4 border-red-200'>
-                        <h1 className=' text-4xl md:text-5xl p-8 font-bold'>
-                            Hi there! I'm
-                        </h1>
-                        <h1 className=' text-4xl md:text-5xl p-8 font-bold'>
-                            Ayush Kumar Gupta
-                        </h1>
-                    </div>
-                    <div className=' mx-auto border-4'>
-                        
-                        <DotLottieReact
-                            src="https://lottie.host/6b12f128-199f-4dec-8414-594148320751/TOIpQ8uucp.lottie"
-                            loop
-                            height={400}
-                            width={300}
-                            autoplay
-                        />
-                    </div>
-                </div>
-            
-        </>
-    )
-}
+        <div className="flex mx-auto flex-col md:flex-row items-center justify-center min-h-screen px-6 py-10">
 
-export default Main
+            {/* Text Section */}
+            <div className="flex flex-col items-center md:items-start text-center md:text-left max-w-xl space-y-4">
+                <TextGenerateEffect className="text-3xl md:text-5xl font-extrabold leading-snug" words={"Hi there! I'm"} />
+                <TextGenerateEffect className="text-3xl md:text-5xl font-extrabold leading-snug text-red-500" words={"Ayush Kumar Gupta"} />
+                <p className="text-violet-300 text-sm md:text-lg leading-relaxed">
+                A problem-solving developer driven by a passion for programming, with an insatiable eagerness to learn and adapt to new technologies continuously.
+                </p>
+            </div>
+
+            {/* Lottie Animation Section */}
+            <div className="w-full md:w-fit max-w-sm md:max-w-md mt-8 md:mt-0">
+                <DotLottieReact
+                    src="https://lottie.host/6b12f128-199f-4dec-8414-594148320751/TOIpQ8uucp.lottie"
+                    loop
+                    className="h-auto w-full"
+                    autoplay
+                    height={400}
+                    width={400}
+                />
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Main;
